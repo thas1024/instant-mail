78aa2d0cb29d0b1e8dc73cb08e04b9004356108d
thascc <thascc1024@gmail.com>
2020-01-06 23:40:44 +0800

    support docker

--- spring-boot-parent/spring-boot-sample/src/main/java/cc/thas/mail/sample/MailScheduleConfiguration.java
+++ spring-boot-parent/spring-boot-sample/src/main/java/cc/thas/mail/sample/MailScheduleConfiguration.java
@@ -1,8 +1,7 @@
 package cc.thas.mail.sample;
 
-import cc.thas.mail.event.impl.MailReceivedEvent;
-import cc.thas.mail.event.listener.AbstractMailReceivedEventListener;
 import cc.thas.mail.event.listener.EventListener;
+import cc.thas.mail.event.listener.impl.DefaultMailReceivedEventListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,26 +9,12 @@ import org.springframework.context.annotation.Configuration;
  * @author <a href="mailto:thascc1024@gmail.com">thas</a>
  * @date 2020/1/5 14:55
  */
-//@Configuration
+@Configuration
 public class MailScheduleConfiguration {
 
     @Bean
     public EventListener customEventListener() {
-        return new AbstractMailReceivedEventListener() {
-            @Override
-            protected void onMailReceived(MailReceivedEvent event) {
-                System.out.println("自定义1" + event.getPayload().getMessage());
-            }
-        };
+        return new DefaultMailReceivedEventListener();
     }
 
-    @Bean
-    public EventListener customEventListener2() {
-        return new AbstractMailReceivedEventListener() {
-            @Override
-            protected void onMailReceived(MailReceivedEvent event) {
-                System.out.println("自定义2" + event.getPayload().getMessage());
-            }
-        };
-    }
 }
